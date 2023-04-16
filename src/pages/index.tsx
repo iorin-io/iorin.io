98280534c21470753633b1919715740e2df9c1a0
iori0705 <iodere77@gmail.com>
2023-04-17 08:48:26 +0900

    import react

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
