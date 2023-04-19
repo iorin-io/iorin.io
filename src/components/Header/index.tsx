6be42e38f8bcaa447f507d0781363d77ee898416
iori0705 <iodere77@gmail.com>
2023-04-19 22:58:30 +0900

    create Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,56 @@
 import Head from 'next/head';
 import Link from 'next/link'
 import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
+import { Style } from '@material-ui/icons';
+
+const StyledHeader = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 40px;
+  z-index: 999;
+  background-color: #C6C6C6;
+  font-size: 30px;
+  color: white;
+  padding-left: 50px;
+`;
+
+const StyledP =styled.p`
+  display: inline-block;
+  font-size: 10px;
+  padding: 5px;
+`
+const StyledDiv = styled.div`
+  display: inline-block;
+  position: absolute;
+  right: 20px;
+`
+
+const StyledA = styled.a`
+  color: white;
+  font-size: 12px;
+`
 
 const Header = () => {
-    return (
-        <header className="border-b flex items-center h-14 px-4">
-            <h1>
-                <Link href="/" legacyBehavior>
-                  <a className="text-2xl font-logo">iam</a>
-                </Link>
-              </h1>
-            </header>
-    )
 
+  return (
+    <StyledHeader>
+      iorin.io
+      <StyledDiv>
+          <StyledP>
+            <StyledA href="https://twitter.com/iori0705" target="_blank">Twitter</StyledA>
+          </StyledP>
+          <StyledP>
+            <StyledA href="https://www.instagram.com/10ri__75/" target="_blank">Instagram</StyledA>
+          </StyledP>
+          <StyledP>
+            <StyledA href="https://github.com/iori0705" target="_blank">GitHub</StyledA>
+          </StyledP>
+      </StyledDiv>
+    </StyledHeader>
+  );
 }
 
 export default Header;
