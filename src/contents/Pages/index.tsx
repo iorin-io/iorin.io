bce1eb5d2a38d499a4c8d99bef474ad2537e616f
iori0705 <iodere77@gmail.com>
2023-04-23 00:21:00 +0900

    create links

--- src/contents/Pages/index.tsx
+++ src/contents/Pages/index.tsx
@@ -23,7 +23,7 @@ const Pages = () => {
             <H1>Pages</H1>
             <StyledDiv>
                 <p><A href='/blog'>Blog</A></p>
-                <p><A href='/'>授業感想</A></p>
+                <p><A href='/class_impressions'>授業感想</A></p>
             </StyledDiv>
         </Margin>
     )
