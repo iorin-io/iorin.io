d1cf3cf17db7de7c219b6b533e5bacdef76d9fb7
iori0705 <iodere77@gmail.com>
2023-05-08 01:11:44 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    runtime: 'edge',
-  }
 }
 
 module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  images: {
+      domains: ["iorin.io"],
+      formats: ["image/webp"],
+  },
+};
