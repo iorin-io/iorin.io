046027d5ee59e535c7460dd9e5c2fef183e662eb
iori0705 <iodere77@gmail.com>
2023-05-08 00:53:18 +0900

    fix edge-runtime

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    runtime: 'edge',
+  }
 }
 
 module.exports = nextConfig
-module.exports = {
-  reactStrictMode: true,
-  images: {
-      domains: ["iorin.io"],
-      formats: ["image/webp"],
-  },
-};
