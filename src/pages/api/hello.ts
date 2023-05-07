7df6a985b6bd3b0e0261cbcbf21139558db5f0ec
iori0705 <iodere77@gmail.com>
2023-05-08 00:53:18 +0900

    fix edge-runtime

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
 
-type Data = {
-  name: string
+export const config = {
+  runtime: 'edge',
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req) {
+  return new Response(JSON.stringify({ name: 'John Doe' }))
 }
