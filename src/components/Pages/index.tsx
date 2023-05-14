2177d36a064aa6def9467423514b37ab353a3ae6
iori0705 <iodere77@gmail.com>
2023-05-14 15:29:10 +0900

    yarn prettier --write src

--- src/components/Pages/index.tsx
+++ src/components/Pages/index.tsx
@@ -1,33 +1,40 @@
-import Header from '@/components/Header';
-import Head from 'next/head';
-import styled from 'styled-components';
+import Header from "@/components/Header";
+import Head from "next/head";
+import styled from "styled-components";
 
 const StyledDiv = styled.div`
-    position: relative;
-    padding-left: 20px
-`
+  position: relative;
+  padding-left: 20px;
+`;
 const Margin = styled.div`
-    margin-left: 20px;
-    margin-right: 20px;
-`
-const H1 =styled.h1`
-    color: #5d6869;
-`
+  margin-left: 20px;
+  margin-right: 20px;
+`;
+const H1 = styled.h1`
+  color: #5d6869;
+`;
 const A = styled.a`
-    color: #97AAAB;
-    text-decoration: line-through;
-`
+  color: #97aaab;
+  text-decoration: line-through;
+`;
 
 const Pages = () => {
-    return (
-        <Margin>
-            <H1>Pages</H1>
-            <StyledDiv>
-                <p><A href='blog'>Blog</A></p>
-                <p><A href='/class_impressions'>授業感想</A></p>
-            </StyledDiv>
-        </Margin>
-    )
-}
+  return (
+    <Margin>
+      <H1>Pages</H1>
+      <StyledDiv>
+        <p>
+          <A href="/diary">Diary</A>
+        </p>
+        <p>
+          <A href="/blog">Blog</A>
+        </p>
+        <p>
+          <A href="/class_impressions">授業感想</A>
+        </p>
+      </StyledDiv>
+    </Margin>
+  );
+};
 
 export default Pages;
--- src/components/Works/index.tsx
+++ src/components/Works/index.tsx
@@ -1,32 +1,36 @@
-import Header from '@/components/Header';
-import Head from 'next/head';
-import styled from 'styled-components';
+import Header from "@/components/Header";
+import Head from "next/head";
+import styled from "styled-components";
 
 const StyledDiv = styled.div`
-    position: relative;
-    padding-left: 20px
-`
+  position: relative;
+  padding-left: 20px;
+`;
 const Margin = styled.div`
-    margin-left: 20px;
-    margin-right: 20px;
-`
-const H1 =styled.h1`
-    color: #5d6869;
-`
+  margin-left: 20px;
+  margin-right: 20px;
+`;
+const H1 = styled.h1`
+  color: #5d6869;
+`;
 const A = styled.a`
-    color: #97AAAB;
-    text-decoration: line-through;
-`
+  color: #97aaab;
+  text-decoration: line-through;
+`;
 const Works = () => {
-    return (
-        <Margin>
-            <H1>Works</H1>
-            <StyledDiv>
-                <p><A href='/photo'>Photo</A></p>
-                <p><A href='/movie'>Movie</A></p>
-            </StyledDiv>
-        </Margin>
-    )
-}
+  return (
+    <Margin>
+      <H1>Works</H1>
+      <StyledDiv>
+        <p>
+          <A href="/photo">Photo</A>
+        </p>
+        <p>
+          <A href="/movie">Movie</A>
+        </p>
+      </StyledDiv>
+    </Margin>
+  );
+};
 
 export default Works;
