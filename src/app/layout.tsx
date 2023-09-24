4815d9aca543f79854b03d21e60ee5976dbcde13
iori0705 <iodere77@gmail.com>
2023-09-24 23:43:43 +0900

    runtimeの設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const runtime = 'edge';
+
 export const metadata: Metadata = {
   title: {
     default: 'iorin.io',
