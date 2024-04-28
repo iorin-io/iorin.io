2bc22171b11d22d91e5d92ce6b7b40378fa972b6
iorin-io <iodere77@gmail.com>
2024-04-29 03:17:45 +0900

    爆破

--- src/pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
-
-export const config = {
-  runtime: "edge",
-};
-
-export default async function handler() {
-  return new Response(JSON.stringify({ name: "John Doe" }));
-}
