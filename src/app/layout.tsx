84900ee8debb9f7a007b47eebb042e0432765764
iorin-io <iodere77@gmail.com>
2024-11-12 18:23:19 +0900

    animationができた！

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { css } from "../../styled-system/css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "iorin-io",
@@ -21,7 +18,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${inter.className} ${background}`}>{children}</body>
+			<body className={`${background}`}>{children}</body>
 		</html>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { css } from "../../styled-system/css";
 import Image from "next/image";
-import background from "../../public/background.webp";
+import background from "/public/background.webp";
 import { Inknut_Antiqua } from "next/font/google";
+import * as motion from "framer-motion/client";
+import { ReactNode } from "react";
 
 const Inknut400 = Inknut_Antiqua({
 	weight: "400",
@@ -9,51 +11,136 @@ const Inknut400 = Inknut_Antiqua({
 });
 
 const backgroundImageStyle = css({
-	position: "absolute",
-	top: 0,
-	left: 0,
+	position: "relative",
 	width: "100%",
 	height: "100%",
-	objectFit: "cover",
 	zIndex: -2,
 });
 const backgroundOverlayStyle = css({
 	position: "absolute",
+	backgroundColor: "rgba(0, 0, 0, 0.41)",
 	top: 0,
 	left: 0,
 	width: "100%",
 	height: "100%",
-	backgroundColor: "rgba(0, 0, 0, 0.41)", // 41%の黒の不透明度
 	zIndex: -1,
+});
+
+const fullScreenSize = css({
+	height: "100dvh",
+	width: "100dvw",
 });
 
 const color = css({
 	color: "#fff",
 });
 
+const unavailableColor = css({
+	color: "#909090",
+});
+
+interface BottomAppearanceProps {
+	children: ReactNode;
+	order: number;
+}
+
+const BottomAppearance: React.FC<BottomAppearanceProps> = ({
+	children,
+	order,
+}) => {
+	return (
+		<motion.div
+			initial={{ opacity: 0, y: 20 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{
+				duration: 0.5,
+				delay: 0.2 * order,
+				ease: "easeOut",
+			}}
+		>
+			{children}
+		</motion.div>
+	);
+};
+
 export default function Home() {
 	return (
-		<div className={`${color} ${Inknut400}`}>
-			<Image
-				className={backgroundImageStyle}
-				src={background}
-				alt="background image"
-			/>
-			<div className={backgroundOverlayStyle} />
-			<div>
-				<a>About</a>
-			</div>
-			<div>
-				<a>Works</a>
-			</div>
-			<div>
-				<a>Blog</a>
-			</div>
-			<div>
-				<a>Photo</a>
-			</div>
-			<div>
-				<a>Contact</a>
+		<div className={`${fullScreenSize} ${color} ${Inknut400.className}`}>
+			<motion.div
+				initial={{
+					opacity: 0,
+				}}
+				animate={{
+					opacity: 1,
+				}}
+				transition={{ duration: 1.5, ease: "easeInOut" }}
+				className={css({
+					width: "100%",
+					height: "100%",
+				})}
+			>
+				<Image
+					className={backgroundImageStyle}
+					src={background}
+					alt="background image"
+					fill
+					style={{ objectFit: "cover" }}
+				/>
+				<div className={backgroundOverlayStyle} />
+			</motion.div>
+			<div
+				className={css({
+					position: "absolute",
+					bottom: 0,
+					display: "flex",
+					justifyContent: "space-between",
+					alignItems: "flex-end",
+					flexDirection: "row",
+					width: "100%",
+					padding: "50px",
+				})}
+			>
+				<div
+					className={css({
+						fontSize: "28px",
+						display: "flex",
+						flexDirection: "column",
+						"& > div": {
+							margin: "25px 0",
+						},
+					})}
+				>
+					<BottomAppearance order={4}>
+						<a className={unavailableColor}>About</a>
+					</BottomAppearance>
+					<BottomAppearance order={5}>
+						<a className={unavailableColor}>Works</a>
+					</BottomAppearance>
+					<BottomAppearance order={6}>
+						<a className={unavailableColor}>Blog</a>
+					</BottomAppearance>
+					<BottomAppearance order={7}>
+						<a className={unavailableColor}>Photo</a>
+					</BottomAppearance>
+					<BottomAppearance order={8}>
+						<a className={unavailableColor}>Contact</a>
+					</BottomAppearance>
+				</div>
+				<div
+					className={css({
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "center",
+						alignItems: "center",
+					})}
+				>
+					<BottomAppearance order={9}>
+						<Image src="/dere.png" alt="iorin" width={180} height={180} />
+					</BottomAppearance>
+					<BottomAppearance order={10}>
+						<h1 className={css({ fontSize: "38px" })}>iorin.io</h1>
+					</BottomAppearance>
+				</div>
 			</div>
 		</div>
 	);
