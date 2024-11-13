d160ba50fd9f49665380ec6a31059085c59f107f
iorin-io <iodere77@gmail.com>
2024-11-13 19:04:31 +0900

    Susを追加

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,41 @@
 "use client";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import ReactMarkdown from "react-markdown";
+import { css } from "../../../styled-system/css";
+
+const containerCss = css({
+	margin: "0 auto",
+	maxWidth: "800px",
+	padding: "32px",
+});
+
+const h2css = css({
+	fontSize: {
+		sm: "32px",
+		base: "20px",
+	},
+	marginTop: "32px",
+	marginBottom: "16px",
+	borderBottom: "2px solid #AFC9BF",
+	paddingBottom: "8px",
+});
+
+const ulCss = css({
+	paddingLeft: "20px",
+	marginBottom: "16px",
+	listStyleType: "none",
+});
+
+const liCss = css({
+	marginBottom: "8px",
+	fontSize: "16px",
+});
+
+const linkCss = css({
+	textDecoration: "none",
+	color: "#2F6F5E",
+});
 
 const MarkdownRenderer = () => {
 	const [fileList, setFileList] = useState<string[]>([]);
@@ -37,7 +71,7 @@ const MarkdownRenderer = () => {
 					throw new Error("Failed to fetch content files");
 				}
 				const data = await response.json();
-				setFileList(data.files as string[]); // データの型を明示
+				setFileList(data.files as string[]);
 			} catch (error) {
 				console.error(error);
 			}
@@ -51,14 +85,17 @@ const MarkdownRenderer = () => {
 			{markdownContent ? (
 				<ReactMarkdown>{markdownContent}</ReactMarkdown>
 			) : (
-				<div>
-					<h2>Markdown Files</h2>
-					<ul>
+				<div className={containerCss}>
+					<h2 className={h2css}>Markdown Files</h2>
+					<ul className={ulCss}>
 						{fileList.map((file, index) => {
 							const fileNameWithoutExt = file.replace(".md", "");
 							return (
-								<li key={index}>
-									<a href={`/blog?article=${fileNameWithoutExt}`}>
+								<li key={index} className={liCss}>
+									<a
+										href={`/blog?article=${fileNameWithoutExt}`}
+										className={linkCss}
+									>
 										{fileNameWithoutExt}
 									</a>
 								</li>
@@ -71,4 +108,10 @@ const MarkdownRenderer = () => {
 	);
 };
 
-export default MarkdownRenderer;
+export default function Page() {
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<MarkdownRenderer />
+		</Suspense>
+	);
+}
