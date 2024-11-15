cf166619f8474c91e6b8cb4e896cc68d40c8ed50
iorin-io <iodere77@gmail.com>
2024-11-15 10:27:55 +0900

    fix

--- src/app/api/blog-files/route.ts
+++ src/app/api/blog-files/route.ts
@@ -6,26 +6,20 @@ export async function GET() {
 	const contentDir = path.join(process.cwd(), "public/blog");
 
 	try {
-		// Markdownファイルを取得し、各ファイルからメタデータを解析
 		const files = fs
 			.readdirSync(contentDir)
 			.filter((file) => file.endsWith(".md"));
 
 		const articles = files.map((file) => {
-			// ファイルのパスと内容を取得
 			const filePath = path.join(contentDir, file);
 			const fileContent = fs.readFileSync(filePath, "utf-8");
 
-			// gray-matterでフロントマターを解析
 			const { data } = matter(fileContent);
 
-			console.log("data", data);
-
-			// スラッグを取得してメタデータとともにオブジェクトとして返す
 			return {
-				slug: file.replace(".md", ""), // ファイル名をスラッグに変換
-				title: data.title || "無題",
-				date: data.date || "日付不明",
+				slug: file.replace(".md", ""),
+				title: data.title || "no title",
+				date: data.date || "no date",
 			};
 		});
 
