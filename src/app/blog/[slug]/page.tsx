6ed2c9999d50c6f65487adc6a8eb04db18fca733
iorin-io <iodere77@gmail.com>
2024-11-15 16:17:21 +0900

    loadingのcss

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,7 +1,12 @@
 export const runtime = "edge";
 import { Suspense } from "react";
 import MarkdownRenderer from "./MarkdownRenderer";
-
+import { css } from "../../../../styled-system/css";
+import { Kiwi_Maru } from "next/font/google";
+const Kiwi400 = Kiwi_Maru({
+	weight: "400",
+	subsets: ["latin"],
+});
 export default async function Page({
 	params,
 }: {
@@ -10,7 +15,22 @@ export default async function Page({
 	const { slug } = await params;
 
 	return (
-		<Suspense fallback={<div>Loading...</div>}>
+		<Suspense
+			fallback={
+				<div
+					className={`${css({
+						height: "100dvh",
+						width: "100dvw",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						color: "#2F6F5E",
+					})} ${Kiwi400.className}`}
+				>
+					<p>Loading...</p>
+				</div>
+			}
+		>
 			<MarkdownRenderer article={slug} />
 		</Suspense>
 	);
