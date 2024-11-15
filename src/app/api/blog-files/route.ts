9195a685e57b2691746221fa8dcd98fdfef1119b
iorin-io <iodere77@gmail.com>
2024-11-15 10:33:52 +0900

    日付降順にソート

--- src/app/api/blog-files/route.ts
+++ src/app/api/blog-files/route.ts
@@ -10,18 +10,21 @@ export async function GET() {
 			.readdirSync(contentDir)
 			.filter((file) => file.endsWith(".md"));
 
-		const articles = files.map((file) => {
-			const filePath = path.join(contentDir, file);
-			const fileContent = fs.readFileSync(filePath, "utf-8");
+		const articles = files
+			.map((file) => {
+				const filePath = path.join(contentDir, file);
+				const fileContent = fs.readFileSync(filePath, "utf-8");
 
-			const { data } = matter(fileContent);
+				const { data } = matter(fileContent);
 
-			return {
-				slug: file.replace(".md", ""),
-				title: data.title || "no title",
-				date: data.date || "no date",
-			};
-		});
+				return {
+					slug: file.replace(".md", ""),
+					title: data.title || "no title",
+					date: data.date || "no date",
+				};
+			})
+			.filter(Boolean)
+			.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
 		return new Response(JSON.stringify({ articles }), {
 			status: 200,
