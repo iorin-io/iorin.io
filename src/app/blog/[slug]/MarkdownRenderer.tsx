824c3733d0648cd765db8fde7aa879bc66f1c404
iorin-io <iodere77@gmail.com>
2024-11-15 14:26:25 +0900

    edge runtimeを修正

--- src/app/blog/[slug]/MarkdownRenderer.tsx
+++ src/app/blog/[slug]/MarkdownRenderer.tsx
@@ -1,5 +1,4 @@
 "use client";
-export const runtime = "edge";
 
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,4 @@
+export const runtime = "edge";
 import { Suspense } from "react";
 import MarkdownRenderer from "./MarkdownRenderer";
 
