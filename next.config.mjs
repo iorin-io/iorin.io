3d9f3f2eda5c0c67a2bc5a16dfd64336ff151e0b
iorin-io <iodere77@gmail.com>
2024-11-29 15:02:57 +0900

    async_hooksを明示的に解決したい

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.md$/,
-            use: "raw-loader",
-        });
-        return config;
-    },
+	webpack: (config) => {
+		config.module.rules.push({
+			test: /\.md$/,
+			use: "raw-loader",
+		});
+		config.resolve.fallback = {
+			...config.resolve.fallback,
+			async_hooks: require.resolve("async_hooks"),
+		};
+		return config;
+	},
 };
 
 export default nextConfig;
